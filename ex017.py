65f43cc956f92653e46a8a2918cae82db6984a4c
davi-of-rivia <davi.francisco214@gmail.com>
2020-02-14 23:47:50 -0300

    Adicionando espaço na linha 5 do ex017

--- ex017.py
+++ ex017.py
@@ -2,7 +2,7 @@
 # resultado
 from math import sqrt
 
-cateto_adj = float(input('Informe a medida do cateto adjacente:'))
+cateto_adj = float(input('Informe a medida do cateto adjacente: '))
 cateto_oposto = float(input('Informe a medida do cateto oposto: '))
 hip = sqrt((cateto_adj ** 2) + (cateto_oposto ** 2))
 print(f'A hipotenusa vale {hip:.2f}')
