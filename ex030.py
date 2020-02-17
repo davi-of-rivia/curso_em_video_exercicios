15d99dfae3d3c332e091cf05058562e6a392fc5f
davi-of-rivia <davi.francisco214@gmail.com>
2020-02-17 19:29:07 -0300

    Par ou Ímpar

---
+++ ex030.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número e mostre se ele é impar ou par.
+num = int(input('Digite um número: '))
+
+if num % 2 == 0:
+    print(f'O número {num} é PAR!')
+else:
+    print(f'O número {num} é ÍMPAR!')
